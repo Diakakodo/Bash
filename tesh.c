621c695eb0b78593e759a448aade183e753eab55
Jerem <Jerem@DESKTOP-OTQ47E0>
2017-11-08 18:01:35 +0100

    prompt

--- tesh.c
+++ tesh.c
@@ -1,4 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAX_LENGTH_INSTRUCTION 128
+
+char* getInstruction(char* sentence) {
+	char* endOfLine = NULL;
+
+	if(fgets(sentence, MAX_LENGTH_INSTRUCTION, stdin) != NULL) {
+		endOfLine = strchr(sentence, '\n');
+
+		if(endOfLine != NULL) {
+			*endOfLine = '\0';
+		}
+
+		return sentence;
+	} else return NULL;
+}
+
 int main() {
     printf("Hello, world!\n");
+	char sentence[MAX_LENGTH_INSTRUCTION];
+
+    while(1) {
+    	printf("%s", getInstruction(sentence));
+    }
     return 0;
 }
